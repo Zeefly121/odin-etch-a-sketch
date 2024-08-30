caaffc5a690ceea72af6cae69b97a8609af0467d
Zeeshan Qureshi <102156601+Zeefly121@users.noreply.github.com>
2024-08-30 17:45:47 +0500

    Add function to darken cell on mouse over
    add logic to increase alpha value of rgba each
    time element is moused over

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,18 @@
 const container = document.querySelector("#container");
-const newGridButton = document.querySelector("#newGrid")
+const newGridButton = document.querySelector("#newGrid");
 
-newGridButton.addEventListener("click", createGrid)
+newGridButton.addEventListener("click", createGrid);
+container.addEventListener("mouseover", (e) => darkenCell(e));
 
 function getUserGridSize () {
-    const userInput = prompt("Please enter the grid size you'd like (ex: 4x2 for 4 rows and 2 columns).")
+    const userInput = prompt("Please enter the grid size you'd like (ex: 4x2 for 4 rows and 2 columns - max 100x100).")
     .split("x");
 
-    return userInput;
+    if (userInput[0] > 100 || userInput[1] > 100) {
+        alert("Please enter a valid number.");
+    } else {
+        return userInput;
+    }
 }
 
 function createGrid () { 
@@ -30,7 +35,17 @@ function createGrid () {
             let cell = document.createElement("div");
 
             cell.classList.add("cells");
+            cell.setAttribute("style", "background-color: rgba(0,0,0,0)")
             row.appendChild(cell);
         }
+    }
+}
+
+function darkenCell (event) {
+    let cellBgColor = event.target.style.backgroundColor.slice(5, -1).split(", ").map(parseFloat);
+
+    if (cellBgColor[3] != 1) {
+        cellBgColor[3] = cellBgColor[3] + 0.1;
+        event.target.style.backgroundColor = `rgba(${cellBgColor[0]},${cellBgColor[1]},${cellBgColor[2]},${cellBgColor[3]})`
     }
 }
